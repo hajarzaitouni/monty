a3404ca756379a7fe132f2b8e4c6d2100e813849
Oumi102000 <o.elmadani10@gmail.com>
2023-07-26 21:31:27 +0000

    implement pint opcodes

--- stack_operations.c
+++ stack_operations.c
@@ -54,3 +54,19 @@ void _pall(stack_t **stack, unsigned int line_number)
 		temp = temp->next;
 	}
 }
+/**
+ * _pint - prints the value at the top of the stack
+ *
+ * @stack: a double pointer to the top of the stack
+ * line_number: the line number in monty file
+ */
+
+void _pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
